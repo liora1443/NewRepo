6a9a438b61235adb2bf015c4f15a9a30e6159088
liora1443 <liora1443@gmail.com>
2015-12-01 01:30:20 +0200

    ll

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("lior");
+            Console.WriteLine("lior2");
         }
     }
 }
